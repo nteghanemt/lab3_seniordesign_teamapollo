be69d8e810344e9520cf2a0b03d98ae3bd014cb4
nteghanemt <nadim.teghanemt@gmail.com>
2021-12-01 15:50:01 -0600

    Can add polls to DB

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,46 +1,35 @@
 // Step 1: Import React
-import { graphql } from 'gatsby'
 import * as React from 'react'
+import database from "../components/firebase";
 // Step 2: Define your component
-const UserPage = ({data}) => {
+
+//calling database.ref will list all polls
+//lab3-doodle-poll-default-rtdb
+
+
+var data = [{}];
+
+
+const tempvar = database.ref("Polls");
+tempvar.on('value', (snapshot) => {
+  const temp3 = snapshot.val();
+  data = temp3;
+  console.log(data.poll5.Day);
+})
+
+
+//console.log("hello");
+//console.log(tempvar);
+
+const UserPage = () => {
   return (
     <main>
       <title>About me</title>
       <h1>View available polls</h1>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-      <table>
-      <tr><td>Name</td><td>Day</td></tr>
-      {data.allPolls.edges.map(({ node, index}) => (
-        <tr>
-          <td>
-            {node.name}
-          </td>
-          <td>
-            {node.day}
-          </td>
-        </tr>
-      ))}
-      </table>
     </main>
   )
 }
 // Step 3: Export your component
 export default UserPage
 
-export const query = graphql`
-  {
-    allPolls {
-      edges {
-        node {
-          day
-          endtime
-          location
-          name
-          notes
-          starttime
-          blocks
-        }
-      }
-    }
-  }
-`
