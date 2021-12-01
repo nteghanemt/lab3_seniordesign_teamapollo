c91ddc5f536d81f5f87caed57841031a9c92cc85
nteghanemt <nadim.teghanemt@gmail.com>
2021-11-30 19:25:03 -0600

    push to Github w/ some database implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,27 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Lab3 Doodle Poll",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-firestore',
+      options: {
+        credential: require('./lab3-doodle-poll-firebase-adminsdk-zz47t-e25958f747.json'),
+        types: [
+          {
+            type: 'Polls',
+            collection: 'Polls',
+            map: doc=> ({
+              name: doc.Name,
+              blocks: doc.Blocks,
+              day: doc.Day,
+              endtime: doc.EndTime,
+              location: doc.Location,
+              notes: doc.Notes,
+              starttime: doc.StartTime
+            })
+          }
+        ]
+      }
+    }
+  ],
 };
--- src/pages/user.js
+++ src/pages/user.js
@@ -1,14 +1,46 @@
 // Step 1: Import React
+import { graphql } from 'gatsby'
 import * as React from 'react'
 // Step 2: Define your component
-const UserPage = () => {
+const UserPage = ({data}) => {
   return (
     <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
+      <title>About me</title>
+      <h1>View available polls</h1>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <table>
+      <tr><td>Name</td><td>Day</td></tr>
+      {data.allPolls.edges.map(({ node, index}) => (
+        <tr>
+          <td>
+            {node.name}
+          </td>
+          <td>
+            {node.day}
+          </td>
+        </tr>
+      ))}
+      </table>
     </main>
   )
 }
 // Step 3: Export your component
 export default UserPage
+
+export const query = graphql`
+  {
+    allPolls {
+      edges {
+        node {
+          day
+          endtime
+          location
+          name
+          notes
+          starttime
+          blocks
+        }
+      }
+    }
+  }
+`
